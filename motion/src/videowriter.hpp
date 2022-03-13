46c3aec8bcfc6454485e0fc403591a35585274a5
Juan Gonzalez <j-gonzalez@email.de>
2022-03-13 23:57:18 +0100

    reduce writer queue MAX. bug fix

--- motion/src/videowriter.hpp
+++ motion/src/videowriter.hpp
@@ -14,7 +14,7 @@ using namespace ofxCv;
 using namespace cv;
 using namespace std;
 
-const uint16_t QUEUE_MAX_SIZE = 150;
+const uint16_t QUEUE_MAX_SIZE = 80;
 
 class Videowriter : public ofThread, public VideoWriter
 {
@@ -29,7 +29,7 @@ class Videowriter : public ofThread, public VideoWriter
         if (!m_processing) {
             // make space for new frames
             if (m_queue.size() > QUEUE_MAX_SIZE) {
-                for (int i = 0; i < m_config.parameters.fps; i++) {
+                for (int i = 0; i < QUEUE_MAX_SIZE / 2; i++) {
                     m_queue.pop();
                 }
                 return;
